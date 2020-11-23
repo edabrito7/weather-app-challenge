87c8a6ff39b185fa6e21b30b7ac9073eb0d36cab
edabrito7 <67392231+edabrito7@users.noreply.github.com>
2020-11-23 15:56:19 -0400

    day-card-list uses Context

--- src/components/day-card-list/day-card-list.js
+++ src/components/day-card-list/day-card-list.js
@@ -1,43 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+
+import CurrentLocationContext from '../../context/current-location/current-location.context';
 
 import DayCard from '../day-card/day-card';
 
 import './day-card-list.css';
 
 const DayCardList = () => {
+    const location = useContext(CurrentLocationContext);
+
+    const {consolidated_weather} = location;
+
+    const NextDays = consolidated_weather.slice(1).map(({min_temp,max_temp,weather_state_abbr, applicable_date}) => {
+        const date = new Date(applicable_date);
+        return(
+            <DayCard
+            date={date.toGMTString().substring(0, 10)}
+            lowerTemperature={min_temp.toFixed(1)}
+            higherTemperature={max_temp.toFixed(1)}
+            icon={weather_state_abbr}
+            />
+        )
+    })
+
     return(
         <div className='day-card-list-container'>
-            <DayCard 
-            date='Tomorrow'
-            lowerTemperature='15'
-            higherTemperature='25'
-            icon='s'
-            />
-            <DayCard 
-            date='Tomorrow'
-            lowerTemperature='15'
-            higherTemperature='25'
-            icon='s'
-            />
-            <DayCard 
-            date='Tomorrow'
-            lowerTemperature='15'
-            higherTemperature='25'
-            icon='s'
-            />
-            <DayCard 
-            date='Tomorrow'
-            lowerTemperature='15'
-            higherTemperature='25'
-            icon='s'
-            />
-            <DayCard 
-            date='Tomorrow'
-            lowerTemperature='15'
-            higherTemperature='25'
-            icon='s'
-            />
+           {NextDays}
         </div>
     )
 }
---
+++ src/utils/utils.js
@@ -0,0 +1,3 @@
+const getFormatedDate = (date) => {
+    
+}
