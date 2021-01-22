d4461ed3554f22cd9672af1ae1618bce91a80357
edabrito7 <67392231+edabrito7@users.noreply.github.com>
2021-01-22 18:48:15 -0400

    modal for unavaliable cities added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 import './App.css';
 
@@ -6,6 +6,7 @@ import Loading from './components/loading/loading';
 import ErrorPage from './components/error-page/error-page';
 import Side from './components/side/side';
 import MainInfo from './components/main-info/main-info';
+import Modal from './components/modal/modal';
 
 import CurrentLocationContext from './context/current-location/current-location.context';
 import {weatherApiContext} from './context/weather-provider/weather.provider';
@@ -16,6 +17,12 @@ import useFetch from './services/searchLocation';
 const App = () => {
   const weatherApi = useContext(weatherApiContext);
   const { currentLocation,  currentQuery } = weatherApi;
+  const [isOpen, setisOpen] = useState(false);
+  const [isNewCity, setisNewCity] = useState(true);
+
+  const onClose = () => {
+    setisOpen(false);
+  } 
 
   const { 
     data: searchData,
@@ -34,9 +41,25 @@ const App = () => {
 
   if ( searchError || locationError ) return <ErrorPage />
 
+  if ( searchData === undefined && isOpen === false && isNewCity === true ) {
+    setisOpen(true);
+    setisNewCity(false);
+  } 
+
+  if (isNewCity === false && searchData !== undefined ) {
+    setisNewCity(true);
+  }
+  
   return (
       <CurrentLocationContext.Provider value={locationData || null}>
         <div className="App">
+          <Modal
+          open={isOpen}
+          close={onClose}
+          >
+            The city you have selected is not avaliable. 
+            Our most sincere apologies.
+          </Modal>
           <Side/>
           <MainInfo/>
         </div>
--- src/components/error-page/error-page.js
+++ src/components/error-page/error-page.js
@@ -8,12 +8,12 @@ const ErrorPage = () => {
         <div className='error-page-container'>
             <h1 className='text'>Sorry something went wrong...</h1>
             <h2 className='text'>It is me.. not you</h2>
-            <button 
+            <div 
             className='text text-link'
             onClick={() => window.location.reload() }
             >
                 let's try again
-            </button>
+            </div>
         </div>
     )
 }
