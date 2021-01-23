ef98c23e4cb57d6d8e7544c832f400b97a16294a
edabrito7 <67392231+edabrito7@users.noreply.github.com>
2021-01-22 20:05:13 -0400

    go back to infoside after searching added

--- src/components/search-place/search-place.js
+++ src/components/search-place/search-place.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 
 import { PlacesSearchedContext } from '../../context/places-searched/places-searched.provider';
 import { weatherApiContext } from '../../context/weather-provider/weather.provider';
+import SearchButtonContext from '../../context/search/search-button.context';
 
 import './search-place.css';
 
 const SearchPlace = () => {
     const placesSearched = useContext(PlacesSearchedContext);
     const weatherContext = useContext(weatherApiContext);
+    const { toggleSearchButton } = useContext(SearchButtonContext);
     
     const { add, currentCity } = placesSearched; 
     const { changeLocation, changeQuery } = weatherContext;
@@ -15,6 +17,7 @@ const SearchPlace = () => {
         add(currentCity);
         changeQuery('search/?query=');
         changeLocation(currentCity);
+        toggleSearchButton();
     }
     return(
         <button 
